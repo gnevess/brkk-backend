6f7fabc53acc239851b5515bba541bd79aceaf86
gnevess <gnevesrossi@gmail.com>
2024-11-23 01:08:25 -0300

    fix: back-end corrections

--- src/giveaway/giveaway.module.ts
+++ src/giveaway/giveaway.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { GiveawayService } from './giveaway.service';
 import { UsersModule } from '../users/users.module';
 import { WebsocketModule } from 'src/websocket/websocket.module';
+import { GiveawayController } from './giveaway.controller';
 
 @Module({
   imports: [
     UsersModule,
     WebsocketModule,
   ],
+  controllers: [GiveawayController],
   providers: [GiveawayService],
   exports: [GiveawayService],
 })
